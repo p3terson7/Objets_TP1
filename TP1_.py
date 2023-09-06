91daba36fb23ad77d50f17ec19d5b2680a45099f
p3terson7 <psarateanu@hotmail.ca>
2023-09-06 15:26:49 -0400

    implémentation des indices

--- TP1_.py
+++ TP1_.py
@@ -21,15 +21,27 @@ class MotMystere:
         mot = random.choice(MotMystere.DICTIONNAIRE)
         lettres_manquantes = [mot[i] for i in range(len(mot))]
 
-        lettres_trouvees = []
         essais = 1
         while GAME_OVER == False:
             print("Essai", essais, "/", MotMystere.NOMBRE_ESSAIS)
             mot_essai = input("Entrez motEssai: ")
+            lettres_essai = [mot_essai[i] for i in range(len(mot_essai))]
+            lettres_trouvees = []
+            lettres_indices = []
 
             if mot_essai == mot:
                 print("Vous avez gagné!")
                 GAME_OVER = True
 
+            for i in range(len(lettres_manquantes)):
+                if lettres_essai[i] == lettres_manquantes[i]:
+                    lettres_indices.append("✔")
+                elif lettres_essai[i] in lettres_manquantes:
+                    lettres_indices.append("\u002B")
+                else:
+                    lettres_indices.append("✘")
+            print(lettres_indices)
+            essais += 1
 
-# MotMystere.__init__()
+
+MotMystere.__init__()
