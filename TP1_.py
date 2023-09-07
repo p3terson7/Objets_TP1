5696b7c081b9513cf3a3af40b7b3668a2f6a410f
p3terson7 <psarateanu@hotmail.ca>
2023-09-07 10:24:56 -0400

    implémentation des plus

--- TP1_.py
+++ TP1_.py
@@ -19,7 +19,7 @@ class MotMystere:
     def __init__():
         GAME_OVER = False
         mot = random.choice(MotMystere.DICTIONNAIRE)
-        lettres_manquantes = [mot[i] for i in range(len(mot))]
+        lettres_mot = [mot[i] for i in range(len(mot))]
 
         essais = 1
         while GAME_OVER == False:
@@ -31,6 +31,7 @@ class MotMystere:
             print("Mot mystère:", mot)
             print("Essai", essais, "/", MotMystere.NOMBRE_ESSAIS)
             mot_essai = input("Entrez motEssai: ")
+
             if len(mot_essai) != len(mot):
                 print("Le mot doit avoir", len(mot), "lettres!\n")
                 continue
@@ -43,13 +44,30 @@ class MotMystere:
                 print("Vous avez gagné!")
                 GAME_OVER = True
 
-            for i in range(len(lettres_manquantes)):
-                if lettres_essai[i] == lettres_manquantes[i]:
+            for i in range(len(lettres_mot)):
+                if lettres_essai[i] == lettres_mot[i]:
                     lettres_indices.append("✔")
-                elif lettres_essai[i] in lettres_manquantes:
-                    lettres_indices.append("\u002B")
+                elif lettres_essai[i] in lettres_mot:
+                    lettres_indices.append("+")
                 else:
                     lettres_indices.append("✘")
+            for i in range(len(lettres_indices)):
+                if lettres_indices[i] == "+":
+                    if lettres_essai.count(lettres_essai[i]) >= lettres_mot.count(
+                        lettres_essai[i]
+                    ):
+                        compteur_memes_lettres = lettres_essai.count(lettres_essai[i])
+                        print(
+                            compteur_memes_lettres,
+                            lettres_essai[i],
+                            lettres_mot.count(lettres_essai[i]),
+                        )
+                        while compteur_memes_lettres >= lettres_mot.count(
+                            lettres_essai[i]
+                        ):
+                            lettres_indices[i] = "✘"
+                            lettres_essai[lettres_essai.index(lettres_essai[i])] = " "
+                            compteur_memes_lettres -= 1
             print(lettres_indices)
             essais += 1
 
